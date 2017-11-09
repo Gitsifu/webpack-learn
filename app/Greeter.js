2749c414a7db536d606df00eff2266fde07a37f1
sifu <2587107273@qq.com>
2017-11-09 14:07:49 +0800

    更新

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,5 +1,15 @@
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
-  return greet;
-};
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './Greeter.css'; //导入
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}> 
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter
